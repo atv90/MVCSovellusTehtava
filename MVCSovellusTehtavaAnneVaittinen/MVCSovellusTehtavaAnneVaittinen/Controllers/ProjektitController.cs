ee815dcb846acbc10c79751d19e57633b1389708
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2017-12-27 11:39:31 +0200

    luotiin ProjektitKontrolleri ja Projektit näkymä sekä tietokantayhteys

---
+++ MVCSovellusTehtavaAnneVaittinen/MVCSovellusTehtavaAnneVaittinen/Controllers/ProjektitController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSovellusTehtavaAnneVaittinen.Controllers
+{
+    public class ProjektitController : Controller
+    {
+        // GET: Projektit
+        public ActionResult Projektit()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCSovellusTehtavaAnneVaittinen/MVCSovellusTehtavaAnneVaittinen/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCSovellusTehtavaAnneVaittinen.MvcApplication" Language="C#" %>
