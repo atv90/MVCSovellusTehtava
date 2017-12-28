032d240e5ce9ff094f8dd36927ec2e1c9a700d6c
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2017-12-28 16:32:31 +0200

    lisättiin projektit näkymään tietojen modaali-ikkuna, joka ilmestyy nappia klikatessa

--- MVCSovellusTehtavaAnneVaittinen/MVCSovellusTehtavaAnneVaittinen/Controllers/ProjektitController.cs
+++ MVCSovellusTehtavaAnneVaittinen/MVCSovellusTehtavaAnneVaittinen/Controllers/ProjektitController.cs
@@ -32,5 +32,43 @@ namespace MVCSovellusTehtavaAnneVaittinen.Controllers
 
             return Json(json,JsonRequestBehavior.AllowGet);
         }
+        public JsonResult GetSingleProjekti(int id)
+        {
+            HarjoitustietokantaEntities entities = new HarjoitustietokantaEntities();
+            var model = (from p in entities.Projektit
+                         where p.ProjektiId == id
+                         select new
+                         {
+                             ProjektiId = p.ProjektiId,
+                             Nimi = p.Nimi
+                         }).FirstOrDefault();
+
+            string json = JsonConvert.SerializeObject(model);
+            entities.Dispose();
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Update(Projektit proj)
+        {
+            HarjoitustietokantaEntities entities = new HarjoitustietokantaEntities();
+            //haetaan tietokannan rivi id:n perusteella
+            int id = proj.ProjektiId;
+
+            bool OK = false;
+            Projektit dbItem = (from p in entities.Projektit
+                                 where p.ProjektiId == id
+                                 select p).FirstOrDefault();
+            //kopioidaan selaimelta saadut tiedot tietokantaan, jos kentän arvo ei ole nolla
+            if (dbItem != null)
+            {
+                dbItem.ProjektiId = proj.ProjektiId;
+                dbItem.Nimi = proj.Nimi;
+
+                entities.SaveChanges();
+                //jos tietojen tallennus onnistuu asetetaan OK = true
+                OK = true;
+            }
+            entities.Dispose();
+            return Json(OK, JsonRequestBehavior.AllowGet);
+        }
     }
 }
