2bdac0ab51915a26701fd26ca4fcb8180f6bea47
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2017-12-29 15:17:23 +0200

    lisättiin tunnit näkymään modaali-ikkuna, ei toimi vielä oikein

--- MVCSovellusTehtavaAnneVaittinen/MVCSovellusTehtavaAnneVaittinen/Controllers/TunnitController.cs
+++ MVCSovellusTehtavaAnneVaittinen/MVCSovellusTehtavaAnneVaittinen/Controllers/TunnitController.cs
@@ -32,12 +32,49 @@ namespace MVCSovellusTehtavaAnneVaittinen.Controllers
             entities.Dispose();
             return Json(json, JsonRequestBehavior.AllowGet);
         }
-        public ActionResult Tu()
+        //modaali-ikkunan luonti
+        public JsonResult GetSingleTunti(int id)
         {
             HarjoitustietokantaEntities entities = new HarjoitustietokantaEntities();
-            List<Tunnit> model = entities.Tunnit.ToList();
+            var model = (from t in entities.Tunnit
+                         where t.TuntiId == id
+                         select new
+                         {
+                             TuntiId = t.TuntiId,
+                             ProjektiId = t.ProjektiId,
+                             HenkiloId = t.HenkiloId,
+                             Pvm = t.Pvm,
+                             Tunti = t.Tunnit1
+                         }).FirstOrDefault();
+
+            string json = JsonConvert.SerializeObject(model);
             entities.Dispose();
-            return View(model);
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        //modaali-ikkunan luonti
+        public ActionResult Update(Tunnit tun)
+        {
+            HarjoitustietokantaEntities entities = new HarjoitustietokantaEntities();
+            int id = tun.TuntiId;
+
+            bool OK = false;
+
+            Tunnit dbItem = (from t in entities.Tunnit
+                             where t.TuntiId == id
+                             select t).FirstOrDefault();
+            if (dbItem != null)
+            {
+                dbItem.ProjektiId = tun.ProjektiId;
+                dbItem.HenkiloId = tun.HenkiloId;
+                dbItem.Pvm = tun.Pvm;
+                dbItem.Tunnit1 = tun.Tunnit1;
+
+                entities.SaveChanges();
+                OK = true;
+            }
+            entities.Dispose();
+            return Json(OK);
         }
     }
 }
