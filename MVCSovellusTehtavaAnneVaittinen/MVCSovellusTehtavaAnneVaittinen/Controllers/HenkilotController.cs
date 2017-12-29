29ee0f8fddda1935c7f53c4dea3c4a7edeaddcce
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2017-12-29 15:50:41 +0200

    lisättiin henkilöt näkymään modaali-ikkuna

--- MVCSovellusTehtavaAnneVaittinen/MVCSovellusTehtavaAnneVaittinen/Controllers/HenkilotController.cs
+++ MVCSovellusTehtavaAnneVaittinen/MVCSovellusTehtavaAnneVaittinen/Controllers/HenkilotController.cs
@@ -33,5 +33,52 @@ namespace MVCSovellusTehtavaAnneVaittinen.Controllers
 
             return Json(json, JsonRequestBehavior.AllowGet);
         }
+
+        public JsonResult GetSingleHenkilo(int id)
+        {
+            HarjoitustietokantaEntities entities = new HarjoitustietokantaEntities();
+            var model = (from h in entities.Henkilot
+                         where h.HenkiloId == id
+                         select new
+                         {
+                             HenkiloId = h.HenkiloId,
+                             Etunimi = h.Etunimi,
+                             Sukunimi = h.Sukunimi,
+                             Osoite = h.Osoite,
+                             Esimies = h.Esimies
+                         }).FirstOrDefault();
+
+            string json = JsonConvert.SerializeObject(model);
+            entities.Dispose();
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Update(Henkilot henk)
+        {
+            HarjoitustietokantaEntities entities = new HarjoitustietokantaEntities();
+            //haetaan tietokannan rivi id:n perusteella
+            int id = henk.HenkiloId;
+
+            bool OK = false;
+
+            Henkilot dbItem = (from h in entities.Henkilot
+                                where h.HenkiloId == id
+                                select h).FirstOrDefault();
+            //kopioidaan selaimelta saadut tiedot tietokantaan, jos kentän arvo ei ole nolla
+            if (dbItem != null)
+            {
+                dbItem.HenkiloId = henk.HenkiloId;
+                dbItem.Etunimi = henk.Etunimi;
+                dbItem.Sukunimi = henk.Sukunimi;
+                dbItem.Sukunimi = henk.Sukunimi;
+                dbItem.Osoite = henk.Osoite;
+                dbItem.Esimies = henk.Esimies;
+
+                entities.SaveChanges();
+                //jos tietojen tallennus onnistuu asetetaan OK = true
+                OK = true;
+            }
+            entities.Dispose();
+            return Json(OK);
+        }
     }
 }
